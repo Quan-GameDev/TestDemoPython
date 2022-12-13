36a6bd7f1186f349e01dbd58ca9d2aab4ba2f5e0
Trần Lê Quân <tranlequan09082001@gmail.com>
2022-12-13 13:19:55 +0700

    Add number

--- TranLeQuan_Bai3.py
+++ TranLeQuan_Bai3.py
@@ -1,6 +1,6 @@
 import base64
 
-base64_message = 'Q1RGe0ZsYWdneVdhZ2d5UmFnZ3l9'
+base64_message = 'eweQ1RGe0ZsYWdneVdhZ2d5UmFnZ3l9'
 base64_bytes = base64_message.encode('ascii')
 message_bytes = base64.b64decode(base64_bytes)
 message = message_bytes.decode('ascii')
